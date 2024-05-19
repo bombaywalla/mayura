db55b7919705d4ede9d5c700a8cd62d26c906537
Dorab Patel <dorabpatel@gmail.com>
2024-05-19 11:19:56 -0700

    Initial copy of website as of 2024/05/19.

---
+++ mayura-indian-restaurant.com/javascript/validation.js
@@ -0,0 +1,13 @@
+
+
+	browser_version= parseInt(navigator.appVersion);
+	browser_type = navigator.appName;
+	
+	if (browser_type == "Microsoft Internet Explorer" && (browser_version >= 4)) {
+	document.write("<link rel='stylesheet' href='css/ie.css' type='text/css'>");
+	}
+	
+	else if (browser_type == "Netscape" && (browser_version >= 4)) {
+	document.write("<link rel='stylesheet' href='css/style.css' type='text/css'>");
+	}
+
