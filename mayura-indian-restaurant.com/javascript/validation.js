d98c6a77b1f4af2c4645f82443272d981be0fab9
Dorab Patel <dorabpatel@gmail.com>
2024-05-19 12:00:52 -0700

    Changed to Unix encoding. Removed blank lines.

--- mayura-indian-restaurant.com/javascript/validation.js
+++ mayura-indian-restaurant.com/javascript/validation.js
@@ -1,13 +1,10 @@
+browser_version= parseInt(navigator.appVersion);
+browser_type = navigator.appName;
 
+if (browser_type == "Microsoft Internet Explorer" && (browser_version >= 4)) {
+document.write("<link rel='stylesheet' href='css/ie.css' type='text/css'>");
+}
 
-	browser_version= parseInt(navigator.appVersion);
-	browser_type = navigator.appName;
-	
-	if (browser_type == "Microsoft Internet Explorer" && (browser_version >= 4)) {
-	document.write("<link rel='stylesheet' href='css/ie.css' type='text/css'>");
-	}
-	
-	else if (browser_type == "Netscape" && (browser_version >= 4)) {
-	document.write("<link rel='stylesheet' href='css/style.css' type='text/css'>");
-	}
-
+else if (browser_type == "Netscape" && (browser_version >= 4)) {
+document.write("<link rel='stylesheet' href='css/style.css' type='text/css'>");
+}
